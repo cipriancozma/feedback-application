18c1e2dfdb52cb38a0a8dc5237836acc66a19780
Ciprian <cozma_ciprian19@yahoo.com>
2021-12-11 15:06:16 +0200

    added a fake backend with json server and same adjustments to the app

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,22 +1,26 @@
 import React, {useContext} from "react";
 import {FeedbackItem} from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
+import { SpinnerCircularSplit } from 'spinners-react';
 
 export function FeedbackList() {
 
-    const {feedback} = useContext(FeedbackContext);
+    const {feedback, isLoading} = useContext(FeedbackContext);
 
-    if(!feedback || feedback.length === 0) {
+    if(!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No feedback at the moment</p>
     }
 
     return (
-        <div className="feedback-list">
-            {
-                feedback.map((el) => {
-                    return <FeedbackItem key={el.id} el={el} />
-                })
-            }
-        </div>
+        isLoading ? <SpinnerCircularSplit style={{margin: "auto", display: "block"}} size={50} thickness={100} speed={50} color="rgba(172, 111, 57, 1)" secondaryColor="rgba(172, 97, 57, 0.44)" /> : (
+            <div className="feedback-list">
+                {
+                    feedback.map((el) => {
+                        return <FeedbackItem key={el.id} el={el} />
+                    })
+                }
+            </div>
+        )
     )
+
 }
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,46 @@
-import React, {useState, createContext} from "react";
-import {v4 as uuidv4} from 'uuid';
-
+import React, {useState, createContext, useEffect} from "react";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: "Item from context",
-            rating: 10
-        }
-    ])
+    const [isLoading, setIsLoading] = useState(true);
+    const [feedback, setFeedback] = useState([])
+
+    useEffect(() => {
+        fetchFeedback();
+    }, [])
+
+    const fetchFeedback = async () => {
+        let result = await fetch("/feedback");
+        let data = await result.json();
+        setFeedback(data);
+        setIsLoading(false);
+    }
 
     const [edit, setEdit] = useState({
         item: {},
         edit: false
     })
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback, ...feedback]);
+    const addFeedback = async (newFeedback) => {
+        const response = await fetch("/feedback", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(newFeedback)
+        })
+        const data = await response.json();
+        setFeedback([data, ...feedback]);
     }
 
 
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if(window.confirm('Are you sure you want to delete this feedback?')) {
+            await fetch(`/feedback/${id}`, {
+                method: "DELETE"
+            })
+
             setFeedback(feedback.filter(el => el.id !== id));
         }
     }
@@ -37,8 +52,18 @@ export const FeedbackProvider = ({ children }) => {
         })
     }
 
-    const updateFeedback = (id, newItem) => {
-        setFeedback(feedback.map(item => item.id === id ? {...item, ...newItem} : item))
+    const updateFeedback = async (id, newItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: "PUT",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newItem)
+        })
+
+        const data = await response.json();
+
+        setFeedback(feedback.map(item => item.id === id ? {...item, ...data} : item))
     }
 
     return <FeedbackContext.Provider value={{
@@ -47,7 +72,8 @@ export const FeedbackProvider = ({ children }) => {
         addFeedback,
         editFeedback,
         edit,
-        updateFeedback
+        updateFeedback,
+        isLoading
     }}>
         {children}
     </FeedbackContext.Provider>
